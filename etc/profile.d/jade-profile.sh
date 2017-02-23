faf9eb9c1c375bc8c9faca4e7ae5bb2f443246b2
Vitor Lopes <vmnlop@gmail.com>
2017-02-23 22:37:43 +0000

    tidy up, add some missing files

---
+++ etc/profile.d/jade-profile.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# set default applications for the dashboard
+
+export JTERMINAL=deepin-terminal
+export JFILEMANAGER=nautilus
+export JSOFTWAREINSTALLER=pamac-install
+export JSETTINGS=manjaro-settings-manager 
---
+++ usr/bin/jade.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/sh
+/opt/jade/jade
