55eb9fa3a90909d112ec2028b831c8a35d6895ae
Vitor Lopes <vmnlop@gmail.com>
2017-02-27 16:06:07 +0000

    add welcome screen to distributor logo

--- etc/profile.d/jade-profile.sh
+++ etc/profile.d/jade-profile.sh
@@ -2,8 +2,9 @@
 
 # set default applications for the dashboard
 
-export JTERMINAL=deepin-terminal
-export JFILEMANAGER=nautilus
-export JSOFTWAREINSTALLER=pamac-manager
-export JSETTINGS=manjaro-settings-manager
-export JBROWSER=chromium
+export JADE_TERMINAL=deepin-terminal
+export JADE_FILE_MANAGER=nautilus
+export JADE_SOFTWARE_INSTALLER=pamac-manager
+export JADE_SETTINGS=manjaro-settings-manager
+export JADE_BROWSER=chromium
+export JADE_DISTRIBUTOR_WELCOME_SCREEN=manjaro-welcome
