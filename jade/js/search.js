228ad6396cfc1a3b4d68d51dc8201c09fb904eaf
Vitor Lopes <vmnlop@gmail.com>
2017-03-05 17:27:45 +0000

    add desktop notifications to search

--- jade/js/search.js
+++ jade/js/search.js
@@ -8,48 +8,48 @@
              itemSelector: '.col',
              transitionDuration: '0.0s'
          });
-     })
+     });
      $("#search").on("keyup", function() {
 
          $('.search-results').empty();
-         $("#search-container #search-msg").remove();
-         searchValue = this.value
+         $("#search-msg").remove();
+         var searchValue = this.value;
 
-             "use strict";
-         if (searchValue.startsWith("$", 0)) {
-             window.location.href = "quik:" + searchValue
-         }
+         //"use strict"; quick shell this is not done yet
+         //if (searchValue.startsWith("$", 0)) {
+         //    window.location.href = "quick:" + searchValue;
+         //} else if
 
-         if (searchValue !== "") {
+         if (searchValue.length !== 0) {
              searchValue.toLocaleLowerCase();
 
              $(".application-wrapper").each(function() {
 
                  if ($(this).html().toLocaleLowerCase().indexOf(searchValue) > -1) {
 
-                     $(this).clone().appendTo(".search-results");
+                     $(this).clone().prependTo(".search-results");
                      $('.search-results').masonry('reloadItems');
                      $('.search-results').masonry('layout');
-                     var elementsNumber = $(".search-results .application-wrapper").length;
 
-                     if (elementsNumber === 1) {
+                     var elementsNumber = $(".search-results .application-wrapper").length;
+                     if (elementsNumber == 1) {
                          $("#search-container").append("<div id='search-msg'>Press ENTER key to launch this application!</div>");
-                     } else if (elementsNumber !== 1) {
-                         $("#search-container #search-msg").remove();
+                     } else if (elementsNumber > 1) {
+                         $("#search-msg").remove();
                          $("#search-container").append("<div id='search-msg'>You have " + elementsNumber + " matches!</div>");
                      }
                  }
              })
-         } else if (searchValue == "") {
-             $('.search-results').empty();
          }
      });
      $("#search").keypress(function(event) {
          var elementsNumber = $(".search-results .application-wrapper").length;
-         var key = event.which || event.keyCode;
-         if (key == 13 && elementsNumber === 1) {
-             launch = $(".search-results .application-wrapper .application-box").attr('href');
-             window.location.href = launch
+         var key = event.which;
+         if (key == 13 && elementsNumber == 1) {
+             var text = "Launching " + $(".search-results h5.application-name").text();
+             notifySend(text);
+             var launch = $(".search-results .application-wrapper .application-box").attr('href');
+             window.location.href = launch;
          }
      })
  })
