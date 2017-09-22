c192bdbbec5be0b6c5b6cb9c3208857e6ab79e5b
Vitor Lopes <vmnlop@gmail.com>
2017-09-22 17:33:51 +0100

    rework

--- jade/js/jade.js
+++ jade/js/jade.js
@@ -1,31 +1,44 @@
 function showDashButton() {
-	$(".dashboard-button").fadeIn("slow");
+  $(".dashboard-button").fadeIn("slow");
 }
+
+function isElementVisible(element) {
+  if ($(element).css('display') == 'none') {
+    return false
+  } else {
+    return true
+  }
+
+};
 
 // search for /home/user/.config/jade/theme/override.css
 function themeOverride(fileName) {
-   var link  = document.createElement('link');
-   link.rel  = 'stylesheet';
-   link.type = 'text/css';
-   link.charset = 'UTF-8';
-   link.href = fileName;
-   document.getElementsByTagName('head')[0].appendChild(link);
+
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.charset = 'UTF-8';
+  link.href = fileName;
+  document.getElementsByTagName('head')[0].appendChild(link);
 }
-          
+
+function backEndGet(python_function) {
+  document.title = python_function
+}
 // show hide applications
 function display(element) {
-  
+
   $.when($(".category-msg, #search-icon, #recent-used-files-msg").fadeOut()).done(function() {
-  $(".category-container, .search-results, #main-dashboard, #recently-used-files, .category-msg, #search-icon, #recent-used-files-msg").hide();
-  $(".category-msg").addClass("animated slideInLeft");
-  $(element).show();
-  grid(element);
+    $(".category-container, .search-results, #main-dashboard, #recently-used-files, .category-msg, #search-icon, #recent-used-files-msg").hide();
+    $(".category-msg").addClass("animated slideInLeft");
+    $(element).show();
+    grid(".grid");
   });
   showDashButton();
 };
 
 function grid(element) {
-	$(element).masonry({
+  $(element).masonry({
     itemSelector: ".col"
   });
 }
@@ -36,57 +49,24 @@ function emptyClass(element) {
 
 //show hide dashboard
 function showDashboard() {
-  $(".category-container, .search-results, .category-msg, #search-icon, #recently-used-files, #recent-used-files-msg, .dashboard-button").hide();
-  $("#main-dashboard").show().css("display', 'block"); // fix, reset display state at the end of animation
-  emptyClass("#background");
-}
+  $.when($("#mini-browser").slideUp("slow")).done(function() {
+      $("#search").animate({
+        width: "50%"
+      });
+      $("#search").attr("placeholder", "Search Applications...");
+      $(".category-container, .search-results, .category-msg, #search-icon, #recently-used-files, #recent-used-files-msg, .dashboard-button").hide();
+      $("#main-dashboard").show().css("display', 'block"); // fix, reset display state at the end of animation
+      emptyClass("#background");
+    });
+};
 
-// send messages to the back end
-function notifySend(msg) {
-  document.title = "notify:" + msg;
-}
-
-// DOCUMENT READY
-$(document).ready(function() {
-	
-  // Init a timeout variable to be used below
-  var dashTimeout = null;
-
-  // Listen for events
-  $(document).on("keypress mousewheel mousemove mousedown", function() {
-
-    // Clear the timeout if it has already been set.
-    // This will prevent the previous task from executing
-    // if it has been less than <MILLISECONDS>
-    clearTimeout(dashTimeout);
-
-    // Make a new timeout set to go off in 30sec this could be a really neat feature or the most annoying one
-    // This depends how fast people read, it might need adjustment
-    dashTimeout = setTimeout(function() {
-      if ($("#main-dashboard").css("display") == 'none') { // don't repeat animation
-        $(".category-container, .search-results, .category-msg, #search-icon, #recently-used-files, #recent-used-files-msg, .dashboard-button").fadeOut("slow", function() {
-          $("#main-dashboard").fadeOut("slow");
-        });
-        $(".category-container, .search-results, .category-msg, #search-icon, #recently-used-files, #recent-used-files-msg, .dashboard-button").promise().done(function() {
-          $("#main-dashboard").fadeIn("slow");
-          setTimeout(function() {
-            emptyClass("#background");
-            $("#main-dashboard").css("display', 'block"); // fix, reset display state at the end of animation
-          }, 1800); // delayed background reset
-        });
-      } else {
-        clearTimeout(dashTimeout);
-      }
-    }, 30000);
-  });
-
-  // focus on hover
-  $(".box.notes").parent().mouseover(function() {
-    $(".notes .content-editable").focus();
-  });
+  // send messages to the back end
+  function notifySend(msg) {
+    backEndGet("notify:" + msg);
+  }
 
   // disk usage colours
-  function diskPercentage() {
+  function getDiskColors() {
     var diskPercentage = $(".disk-percentage").text();
     if (diskPercentage >= "90%") {
       $(".disk-usage").css("border-color", "rgba(244, 67, 54, 0.5)"); // red
@@ -96,145 +76,267 @@ $(document).ready(function() {
       $(".disk-usage").css("border-color", "rgba(76, 175, 80, 0.5"); // green
     }
   }
-  diskPercentage();
 
-  // double click for shutdown
-  $(function($) {
-    $("#exit-menu a").click(function() {
-      notifySend("Double click to use!");
-      return false;
-    }).dblclick(function() {
-      window.location = this.href;
-      return false;
-    }).keydown(function(event) {
-      switch (event.which) {
-        case 13: // Enter
-          window.location = this.href;
-          return false;
+  // DOCUMENT READY
+  $(document).ready(function() {
+
+    backEndGet("disk-usage");
+    $('.modal').modal();
+    $("#app-name").text(jadeApplication.name);
+    $("#app-author").text("Author - " + jadeApplication.author);
+    $("#app-description").text(jadeApplication.description);
+    $("#app-url").append("<span>Website - <a href='xdg-open:" + jadeApplication.url + "'>" + jadeApplication.url + "</a></span>");
+    $("#app-version").text("Version - " + jadeApplication.version);
+    $("#app-license").text("License - " + jadeApplication.license);
+
+    // Init a timeout variable to be used below
+    var dashTimeout = null;
+
+    // Listen for events
+    $(document).on("keypress mousewheel mousemove mousedown", function() {
+
+      // Clear the timeout if it has already been set.
+      // This will prevent the previous task from executing
+      // if it has been less than <MILLISECONDS>
+      clearTimeout(dashTimeout);
+
+      // Make a new timeout set to go off in 30sec this could be a really neat feature or the most annoying one
+      // This depends how fast people read, it might need adjustment
+      if (isElementVisible("#go-online")) {
+         console.log("Mini browser open cant go back to main screen");
+      } else {
+        dashTimeout = setTimeout(function() {
+          if ($("#main-dashboard").css("display") == 'none') { // don't repeat animation
+            $(".category-container, .search-results, .category-msg, #search-icon, #recently-used-files, #recent-used-files-msg, .dashboard-button").fadeOut("slow", function() {
+              $("#main-dashboard").fadeOut("slow");
+            });
+            $(".category-container, .search-results, .category-msg, #search-icon, #recently-used-files, #recent-used-files-msg, .dashboard-button").promise().done(function() {
+              $("#main-dashboard").fadeIn("slow");
+              setTimeout(function() {
+                emptyClass("#background");
+                $("#main-dashboard").css("display', 'block"); // fix, reset display state at the end of animation
+                $("#search").animate({
+                  width: "50%"
+                });
+              }, 1800); // delayed background reset
+            });
+          } else {
+            clearTimeout(dashTimeout);
+          }
+        }, 30000);
       }
     });
-  });
 
-  
+    // double click for shutdown
+    $(function($) {
+      $("#exit-menu a").click(function() {
+        notifySend("Double click to use!");
+        return false;
+      }).dblclick(function() {
+        window.location = this.href;
+        return false;
+      }).keydown(function(event) {
+        switch (event.which) {
+          case 13: // Enter
+            window.location = this.href;
+            return false;
+        }
+      });
+    });
 
-  // move to correct containers
-  $(".application-category").appendTo(".menu");
-  $(".category-container").appendTo(".dashboard");
-  $(".category-msg, #search-icon").appendTo(".msg");
-  $("#recent-used-files-msg").appendTo(".msg");
+    // move to correct containers, and add classes
+    $(".application-category").appendTo(".menu");
+    $(".category-container").appendTo(".dashboard");
+    $(".category-msg, #search-icon").appendTo("#TOP-LEFT-DESCRIPTION");
+    $("#recent-used-files-msg").appendTo("#TOP-LEFT-DESCRIPTION").addClass("animated slideInLeft");
+    $(".application-wrapper").addClass("col l4 xl3");
+    $(".application-box").addClass("card");
+    $("#mini-browser-btn, #news").appendTo("#go-online-favorites");
+    $("#manjaro, #wiki, #forum").appendTo("#help-favorites");
+    // help category comes first!
+    $(".application-category.help").prependTo("#nav-mobile");
 
-  // help category comes first!
-  $(".application-category.help").prependTo("#nav-mobile");
+    // settings & system
+    $(".application-category.settings, .application-category.system").appendTo(".mini-dashboard-left");
+    $(".application-category.settings a, .application-category.system a").addClass("box col m12");
+    $(".application-category.settings, .application-category.system").removeClass("application-category");
+    $(".category-icon.Settings").clone().appendTo(".settings a");
+    $(".category-icon.System").clone().appendTo(".system a");
+    emptyClass(".settings a img");
+    emptyClass(".system a img");
 
-  // settings & system
-  $(".application-category.settings, .application-category.system").addClass("col m12").appendTo(".mini-dashboard-left");
-  $(".application-category.settings a, .application-category.system a").addClass("box col m12");
-  $(".application-category.settings, .application-category.system").removeClass("application-category");
-  $(".category-icon.Settings").clone().appendTo(".settings a");
-  $(".category-icon.System").clone().appendTo(".system a");
-  emptyClass(".settings a img");
-  emptyClass(".system a img");
+    // application info slider and icon transition
+    $(".application-box").hover(function() {
+      $(this).find(".application-icon").fadeOut("slow");
+      $(this).find(".info-icon").fadeIn("slow");
+      $(".application-box").css("z-index", "1");
+      $(this).css("z-index", "9");
+      $(this).find("p").slideDown("fast");
 
-  // application info slider and icon transition
-  $(".application-box").hover(function() {
-    $(this).find(".application-icon").fadeOut("slow");
-    $(this).find(".info-icon").fadeIn("slow");
-    $(".application-box").css("z-index", "1");
-    $(this).css("z-index", "2");
-    $(this).find("p").slideDown("fast");
+    }, function() {
+      $(this).find(".info-icon").fadeOut("slow");
+      $(this).find(".application-icon").fadeIn("slow");
+      $(this).find("p").slideUp("fast");
+    });
 
-  }, function() {
-    $(this).find(".info-icon").fadeOut("slow");
-    $(this).find(".application-icon").fadeIn("slow");
-    $(this).find("p").slideUp("fast");
-  });
+    // exit menu animation
+    $("#exit-button").click(function() {
+      $("#exit-menu").parent().find("div").slideToggle("slow");
+    });
 
-  // exit menu animation
-  $("#exit-button").hover(function() {
-    $("#exit-menu").parent().find("div").slideToggle("slow");
-  });
+    $("nav a, #clear-search").hover(function() {
+      $(this).addClass("underline pointer");
+    }, function() {
+      $(this).removeClass("underline pointer");
+    });
 
-  var red = "#f44336";
-  var pink = "#e91e63";
-  var blue = "#2196f3";
-  var teal = "#009688";
-  var green = "#4caf50";
-  var yellow = "#ffeb3b";
-  var orange = "#ff9800";
-  var brown = "#795548";
-  var grey = "#9e9e9e";
-  var colors = [red, blue, pink, green, orange];
+    var red = "#f44336";
+    var pink = "#e91e63";
+    var blue = "#2196f3";
+    var teal = "#009688";
+    var green = "#4caf50";
+    var yellow = "#ffeb3b";
+    var orange = "#ff9800";
+    var brown = "#795548";
+    var grey = "#9e9e9e";
+    var colors = [red, blue, pink, green, orange];
 
-  // apply colors to application container background
-  $('.application-box').css('background-color', function(index) {
-    return colors[index % colors.length];
-  });
+    // apply colors to application container background
+    $('.application-box').css('background-color', function(index) {
+      return colors[index % colors.length];
+    });
 
-  $(".recent-files-button").click(function() {
-  	   showDashButton();
+    $(".recent-files-button").click(function() {
+      showDashButton();
       $("#main-dashboard").hide();
+      $("#search").animate({
+        width: "50%"
+      });
       $("#recent-used-files-msg").addClass("animated slideInLeft");
       $("#recently-used-files, #recent-used-files-msg").show();
-      document.title = "recent";
+      backEndGet("recent-files");
+    });
+
+    $(".dashboard-button a").click(function() {
+      emptyClass("#background");
+      showDashboard();
+    });
+    // backgrounds
+    $("li.application-category").mouseover(function() {
+
+      if ($(this).hasClass("office")) {
+        emptyClass("#background");
+        $("#search").attr("placeholder", "Search Applications...");
+        $("#search").animate({
+          width: "50%"
+        });
+        $("#mini-browser").slideUp("slow");
+        $("#background").addClass("office-background");
+
+      } else if ($(this).hasClass("development")) {
+        $("#search").attr("placeholder", "Search Applications...");
+        $("#search").animate({
+          width: "50%"
+        });
+        $("#mini-browser").slideUp("slow");
+        emptyClass("#background");
+        $("#background").addClass("development-background");
+
+      } else if ($(this).hasClass("education")) {
+        $("#search").attr("placeholder", "Search Applications...");
+        $("#search").animate({
+          width: "50%"
+        });
+        $("#mini-browser").slideUp("slow");
+        emptyClass("#background");
+        $("#background").addClass("education-background");
+        $("#search").attr("placeholder", "Search Applications...");
+        $("#search").animate({
+          width: "50%"
+        });
+
+      } else if ($(this).hasClass("multimedia")) {
+        $("#search").attr("placeholder", "Search Applications...");
+        $("#search").animate({
+          width: "50%"
+        });
+        $("#mini-browser").slideUp("slow");
+        emptyClass("#background");
+        $("#background").addClass("multimedia-background");
+
+      } else if ($(this).hasClass("gaming")) {
+        $("#search").attr("placeholder", "Search Applications...");
+        $("#search").animate({
+          width: "50%"
+        });
+        $("#mini-browser").slideUp("slow");
+        emptyClass("#background");
+        $("#background").addClass("games-background");
+
+      } else if ($(this).hasClass("graphics")) {
+        $("#search").attr("placeholder", "Search Applications...");
+        $("#search").animate({
+          width: "50%"
+        });
+        $("#mini-browser").slideUp("slow");
+        emptyClass("#background");
+        $("#background").addClass("graphics-background");
+
+      } else if ($(this).hasClass("go-online")) {
+
+        emptyClass("#background");
+        $("#background").addClass("internet-background");
+        $("#search").animate({
+          width: "100%"
+        });
+        $("#search").attr("placeholder", "Search The Internet...");
+
+      } else if ($(this).hasClass("help")) {
+        $("#search").attr("placeholder", "Search Applications...");
+        $("#search").animate({
+          width: "50%"
+        });
+        $("#mini-browser").slideUp("slow");
+        emptyClass("#background");
+        $("#background").addClass("help-background");
+
+      } else if ($(this).hasClass("accessories")) {
+        $("#search").attr("placeholder", "Search Applications...");
+        $("#search").animate({
+          width: "50%"
+        });
+        $("#mini-browser").slideUp("slow");
+        emptyClass("#background");
+        $("#background").addClass("accessories-background");
+
+      } else {
+        emptyClass("#background");
+      }
+
+    });
+
+    $(".settings, .system, .recent-files-button").click(function() {
+      $("#search").attr("placeholder", "Search Applications...");
+      $("#search").animate({
+        width: "50%"
+      });
+      $("#mini-browser").slideUp("slow");
+      if ($(this).hasClass("settings")) {
+        emptyClass("#background");
+        $("#background").addClass("settings-background");
+
+      } else if ($(this).hasClass("system")) {
+        $("#background").addClass("system-background");
+
+      } else if ($(this).hasClass("recent-files-button")) {
+        emptyClass("#background");
+        $("#background").addClass("recent-files-background");
+
+      } else {
+        emptyClass("#background");
+      }
+    });
+    $("#mini-browser-btn").click(function() {
+      $("#mini-browser").slideToggle("slow");
+    });
   });
-
-  $(".dashboard-button a").click(function() {
-    emptyClass("#background");
-    showDashboard();
-
-  });
-  // backgrounds
-  $("li.application-category, #search").mouseover(function() {
-
-    if ($(this).hasClass("office")) {
-      emptyClass("#background");
-      $("#background").addClass("office-background");
-    } else if ($(this).hasClass("development")) {
-      emptyClass("#background");
-      $("#background").addClass("development-background");
-    } else if ($(this).hasClass("education")) {
-      emptyClass("#background");
-      $("#background").addClass("education-background");
-    } else if ($(this).hasClass("multimedia")) {
-      emptyClass("#background");
-      $("#background").addClass("multimedia-background");
-    } else if ($(this).hasClass("gaming")) {
-      emptyClass("#background");
-      $("#background").addClass("games-background");
-    } else if ($(this).hasClass("graphics")) {
-      emptyClass("#background");
-      $("#background").addClass("graphics-background");
-    } else if ($(this).hasClass("go-online")) {
-      emptyClass("#background");
-      $("#background").addClass("internet-background");
-    } else if ($(this).hasClass("help")) {
-      emptyClass("#background");
-      $("#background").addClass("help-background");
-    } else if ($(this).hasClass("accessories")) {
-      emptyClass("#background");
-      $("#background").addClass("accessories-background");
-    } else if ($(this).is("#search")) {
-      emptyClass("#background");
-      $("#background").addClass("search-background");
-    } else {
-      emptyClass("#background");
-    }
-
-  });
-
-  $(".settings, .system, .recent-files-button").click(function() {
-
-    if ($(this).hasClass("settings")) {
-      emptyClass("#background");
-      $("#background").addClass("settings-background");
-    } else if ($(this).hasClass("system")) {
-      $("#background").addClass("system-background");
-    } else if ($(this).hasClass("recent-files-button")) {
-      emptyClass("#background");
-      $("#background").addClass("recent-files-background");
-    } else {
-      emptyClass("#background");
-    }
-
-  });
-});
--- jade/js/search.js
+++ jade/js/search.js
@@ -7,15 +7,25 @@ $(document).ready(function() {
     $("input#search").focus();
   });
 
-  $("input#search").on("click mouseover", function() {
+  $("input#search").on("mouseover", function() {
     $("input#search").focus();
-    $.when($(".category-msg, .recent-files-msg, #recent-used-files-msg").fadeOut()).done(function() {
-      $(".category-container, #main-dashboard, #recently-used-files").hide();
-      $("#search-icon").addClass("animated slideInLeft");
-      $(".search-results, #search-icon").show();
-      $(".dashboard-button").fadeIn();
-      grid(".search-results");
-   });
+
+    if (isElementVisible("#go-online")) {
+      $("#search").attr("placeholder", "Search The Internet...");
+      $("#mini-browser").slideDown("slow");
+    } else {
+      $.when($(".category-msg, .recent-files-msg, #recent-used-files-msg").fadeOut()).done(function() {
+        $("#search").attr("placeholder", "Search Applications...");
+        $("#search").animate({
+          width: "50%"
+        });
+        $(".category-container, #main-dashboard, #recently-used-files").hide();
+        $("#search-icon").addClass("animated slideInLeft");
+        $(".search-results, #search-icon").show();
+        $(".dashboard-button").fadeIn();
+        grid(".search-results");
+      });
+    };
   });
 
   // Init a timeout variable to be used below
@@ -36,58 +46,74 @@ $(document).ready(function() {
     // This depends how fast people type, it should be ok even for slow typers
     timeout = setTimeout(function() {
 
-      $(".search-results").empty();
-      $("#search-msg").remove();
-      
       if (searchValue.length !== 0) {
-        searchValue = searchValue.toLocaleLowerCase();
 
-        // search title description and keywords
-        var searchLocation = ".application-wrapper"
+        $(".search-results").empty();
+        $("#search-msg").remove();
 
-        // if we have only 1 letter search tittle only
-        if (searchValue.length == 1) {
-          searchLocation = ".application-wrapper h5"
+        if (isElementVisible("#go-online")) {
+          // internet search
+          var url = 'https://duckduckgo.com/?q='
+          var query = url + searchValue
+          document.getElementById("duckduckgo").contentWindow.location.href = query;
+          notifySend("Searching Term -> " + searchValue);
+          $("#mini-browser").slideDown("slow");
+
+        } else {
+          app_search(searchValue);
         };
 
-        $(searchLocation).each(function() {
+        function app_search(searchValue) {
 
-          if ($(this).text().toLocaleLowerCase().indexOf(searchValue) > -1) {
+          searchValue = searchValue.toLocaleLowerCase();
 
-            if (searchValue.length == 1) {
+          // search title description and keywords
+          var searchLocation = ".application-wrapper"
 
-              if ($(this).text().toLocaleLowerCase().charAt(0) == searchValue) {
-                $(this).parent().parent().clone().prependTo(".search-results");
-              };
-            } else {
+          // if we have only 1 letter search tittle only
+          if (searchValue.length == 1) {
+            searchLocation = ".application-wrapper h5"
+          };
 
-              if ($(this).text().toLocaleLowerCase().trim().startsWith(searchValue)) {
-                // More relevant results first
-                $(this).clone().prependTo(".search-results");
+          $(searchLocation).each(function() {
+
+            if ($(this).text().toLocaleLowerCase().indexOf(searchValue) > -1) {
+
+              if (searchValue.length == 1) {
+
+                if ($(this).text().toLocaleLowerCase().charAt(0) == searchValue) {
+                  $(this).parent().parent().clone().prependTo(".search-results");
+                };
               } else {
-              // Less relevant results last
-              $(this).clone().appendTo(".search-results");
-            }
-          }
 
-            $(".search-results").masonry("reloadItems").masonry("layout");
+                if ($(this).text().toLocaleLowerCase().trim().startsWith(searchValue)) {
+                  // More relevant results first
+                  $(this).clone().prependTo(".search-results");
+                } else {
+                  // Less relevant results last
+                  $(this).clone().appendTo(".search-results");
+                }
+              }
 
-            var elementsNumber = $(".search-results " + searchLocation).length;
+              $(".search-results").masonry("reloadItems").masonry("layout");
 
-            if (elementsNumber === 1) {
+              var elementsNumber = $(".search-results " + searchLocation).length;
+
+              if (elementsNumber === 1) {
+                $("#search-msg").remove();
+                $("#search-container").append("<div id='search-msg'>Press ENTER Key To Launch This Application!</div>");
+
+              } else if (elementsNumber > 1) {
+                $("#search-msg").remove();
+                $("#search-container").append("<div id='search-msg'>You Have " + elementsNumber + " Matches!</div>");
+              }
+
+            } else if ($('.search-results').children().length === 0) {
               $("#search-msg").remove();
-              $("#search-container").append("<div id='search-msg'>Press ENTER Key To Launch This Application!</div>");
-
-            } else if (elementsNumber > 1) {
-              $("#search-msg").remove();
-              $("#search-container").append("<div id='search-msg'>You Have " + elementsNumber + " Matches!</div>");
+              $("#search-container").append("<div id='search-msg'>Sorry No Matches Found!</div>");
             }
-
-          } else if ($('.search-results').children().length === 0) {
-            $("#search-msg").remove();
-            $("#search-container").append("<div id='search-msg'>Sorry No Matches Found!</div>");
-          }
-        });
+          });
+        }
       }
     }, 1000);
   });
