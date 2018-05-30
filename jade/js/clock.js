ae7e9ec3864826d45a2eef9d24150805b72d0d45
Vitor Lopes <vlopes@gmail.com>
2018-05-30 13:22:42 +0100

    minor clean up + wallpapers

--- jade/js/clock.js
+++ jade/js/clock.js
@@ -2,7 +2,7 @@ function timeDate() {
     var today = new Date();
     document.getElementById("time").innerHTML = today.toTimeString().slice(0, 8);
     document.getElementById("date").innerHTML = today.toDateString();
-    var updateTime = setTimeout("timeDate()", 200);
+    setTimeout("timeDate()", 200);
 }
 
 window.onload = timeDate;
